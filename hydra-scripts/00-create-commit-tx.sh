6a41774568dc490a650ffa79f50d7fb40ea1a9d5
Juan Salvador Magan Valero <jmaganvalero @gmail.com>
2024-03-27 15:02:32 +0000

    filter test txs

--- hydra-scripts/00-create-commit-tx.sh
+++ hydra-scripts/00-create-commit-tx.sh
@@ -1,4 +1,5 @@
 #! /bin/bash -i
+LOVELACE=1000000
 
 echo "Put some funds in the address: $(cat ../hydra-node-plutusv3/credentials/cardano-funds-1.addr)"
 
@@ -7,8 +8,12 @@ cardano-cli query utxo \
   --output-json \
   --testnet-magic 4 > cardano-funds-1-commit-utxo.json
 
+cat cardano-funds-1-commit-utxo.json | \
+  jq "with_entries(select(.value.value.lovelace != ${LOVELACE}))" \
+  > cardano-funds-1-commit-utxo-filtered.json
+
 curl -X POST 127.0.0.1:4001/commit \
-  --data @cardano-funds-1-commit-utxo.json \
+  --data @cardano-funds-1-commit-utxo-filtered.json \
   > cardano-funds-1-commit-tx.json
 
 cardano-cli transaction sign \
